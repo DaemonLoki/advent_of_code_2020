0e63af93d126e6c13966d576f5be1feb65da023e
Stefan Blos <stefan.blos@gmail.com>
2020-12-04 19:14:01 +0100

    (Day 3) finished the second puzzle

--- advent-of-code.playground/Pages/day_3.xcplaygroundpage/Contents.swift
+++ advent-of-code.playground/Pages/day_3.xcplaygroundpage/Contents.swift
@@ -6,22 +6,36 @@ var content = loadPuzzleInput(day: "3")!
 
 var slopes = content.split(separator: "\n").compactMap { Array(String($0)) }
 
-let xIncrease = 3
-let yIncrease = 1
-
-let endY = slopes.count
-
-var currentXPosition = 0
-var currentYPosition = 0
-var treeCount = 0
-for y in 1 ..< endY {
-    currentXPosition += xIncrease
-    currentXPosition = currentXPosition % slopes[0].count
-    if String(slopes[y][currentXPosition]) == "#" {
-        treeCount += 1
+func findNumberOfTrees(on slope: [[String.Element]], rightStep: Int, downwardsStep: Int) -> Int {
+    let slopeWidth = slopes[0].count
+    let slopeHeight = slopes.count
+    
+    var currentXPosition = 0
+    var currentYPosition = 0
+    var treeCount = 0
+    
+    while currentYPosition < slopeHeight {
+        currentXPosition = (currentXPosition + rightStep) % slopeWidth
+        currentYPosition += downwardsStep
+        if currentYPosition >= slopeHeight { break }
+        if String(slopes[currentYPosition][currentXPosition]) == "#" {
+            treeCount += 1
+        }
+        
     }
+    return treeCount
 }
 
-print("Encountered \(treeCount) trees.")
+/* Puzzle 1 */
+let rightIncrease = 3
+let downIncrease = 1
+
+print("Encountered \(findNumberOfTrees(on: slopes, rightStep: rightIncrease, downwardsStep: downIncrease)) trees.")
+
+/* Puzzle 2 */
+let stepTuples = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+let endResult = stepTuples.map { findNumberOfTrees(on: slopes, rightStep: $0.0, downwardsStep: $0.1) }.reduce(1, { $0 * $1 })
+
+print("Product of all trees encountered is \(endResult)")
 
 //: [Next](@next)
